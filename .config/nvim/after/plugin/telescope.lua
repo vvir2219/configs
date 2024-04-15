31f958d8cef4119441586272a7d2005c0ecc3c74
Vitca Vlad <vilie0215@gmail.com>
2024-04-15 23:32:21 +0300

    indent

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -14,9 +14,9 @@ require("telescope").setup({
 vim.keymap.set('n', '<C-f>', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>rg', builtin.grep_string, {})
 vim.keymap.set('n', '<C-p>', function()
-	if not pcall(builtin.git_files) then
-		builtin.find_files()
-	end
+    if not pcall(builtin.git_files) then
+        builtin.find_files()
+    end
 end)
 vim.keymap.set('n', '<C-b>', builtin.buffers, {})
 vim.keymap.set('n', '<leader>tk', builtin.keymaps, {})
