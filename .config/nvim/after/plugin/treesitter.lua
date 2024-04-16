cb545e0bb195235be5bf851ffa3cf6f1e67a0b3a
Vitca Vlad <vilie0215@gmail.com>
2024-04-17 01:09:35 +0300

    treesitter something config

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -8,6 +8,8 @@ require'nvim-treesitter.configs'.setup {
   -- Automatically install missing parsers when entering buffer
   -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
   auto_install = true,
+  modules = {},
+  ignore_install = {},
 
   highlight = {
     enable = true,
