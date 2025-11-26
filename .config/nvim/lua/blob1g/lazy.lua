96e52aa6ff12a016233b06660af6565ea11db476
Vitca Vlad <vilie0215@gmail.com>
2025-11-26 20:47:19 +0200

    lazydev

--- .config/nvim/lua/blob1g/lazy.lua
+++ .config/nvim/lua/blob1g/lazy.lua
@@ -129,15 +129,21 @@ require('lazy').setup({
 
       snippets = { preset = 'luasnip' },
       sources = {
-        default = { 'lsp', 'path', 'snippets', 'buffer' },
+        default = { 'lazydev', 'lsp', 'path', 'snippets', 'buffer' },
+        providers = {
+          lazydev = {
+            name = "LazyDev",
+            module = "lazydev.integrations.blink",
+            -- make lazydev completions top priority (see `:h blink.cmp`)
+            score_offset = 100,
+          },
+        },
       },
 
       fuzzy = { implementation = "prefer_rust_with_warning" }
     },
     opts_extend = { "sources.default" }
   },
-
-  { "folke/neodev.nvim",     opts = {} },
 
   { "honza/vim-snippets" },
 
@@ -254,5 +260,19 @@ require('lazy').setup({
 
   {
     'junegunn/vim-easy-align',
-  }
+    init = function ()
+    end
+  },
+
+  {
+    "folke/lazydev.nvim",
+    ft = "lua", -- only load on lua files
+    opts = {
+      library = {
+        -- See the configuration section for more details
+        -- Load luvit types when the `vim.uv` word is found
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+      },
+    },
+  },
 })
