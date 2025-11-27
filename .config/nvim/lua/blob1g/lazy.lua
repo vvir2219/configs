3c0b7b8d5e3596926d0c529fca1ea1f3d2945796
Vitca Vlad <vilie0215@gmail.com>
2025-11-27 21:54:47 +0200

    cleanup; can use v / V in visual mode

--- .config/nvim/lua/blob1g/lazy.lua
+++ .config/nvim/lua/blob1g/lazy.lua
@@ -148,20 +148,6 @@ require('lazy').setup({
   { "honza/vim-snippets" },
 
   { 'echasnovski/mini.nvim', version = false },
-  {
-    "folke/flash.nvim",
-    event = "VeryLazy",
-    -- @type Flash.Config
-    opts = {
-      modes = {
-        char = { enabled = false },
-      }
-    },
-    -- stylua: ignore
-    keys = {
-      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-    },
-  },
 
   {
     'stevearc/oil.nvim',
